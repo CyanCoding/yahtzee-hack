e5e18c1223069214f2fe21ef3fd19a73d29d6f58
Camden Mac Leod <camden.macleod@pm.me>
2021-12-25 17:02:50 -0600

    Convert string to array of 5 numbers

--- dice.go
+++ dice.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+var dice [5]int
 
 func InputDice() {
-	fmt.Print("Please enter your dice ('34531') > ")
-	
+	var input string
+	for len(input) != 5 {
+		fmt.Print("Please enter your dice ('34531') > ")
+		_, _ = fmt.Scanln(&input)
+
+		if len(input) != 5 {
+			fmt.Println("You should have 5 dice!")
+		}
+	}
+
+	// Add to global dice array
+	for i := 0; i < 5; i++ {
+		dice[i] = int(input[i] - '0')
+	}
 }
