fa1a17f72d34b79acd67ec3d16f435278c77394a
Camden Mac Leod <camden.macleod@pm.me>
2021-12-29 10:01:59 -0600

    Replace ordered map with vector of struct

--- scoreboard.go
+++ scoreboard.go
@@ -2,108 +2,114 @@ package main
 
 import (
 	"fmt"
-	"github.com/elliotchance/orderedmap"
 	"github.com/fatih/color"
 )
 
-func GenerateBoard() *orderedmap.OrderedMap {
-	board := orderedmap.NewOrderedMap()
+type ScoreItem struct {
+	name   string
+	points int
+}
 
-	board.Set("1's", 0)
-	board.Set("2's", 0)
-	board.Set("3's", 0)
-	board.Set("4's", 0)
-	board.Set("5's", 0)
-	board.Set("6's", 0)
-	board.Set("Three-of-a-kind", 0)
-	board.Set("Four-of-a-kind", 0)
-	board.Set("Full house", 0)
-	board.Set("Small straight", 0)
-	board.Set("Large straight", 0)
-	board.Set("Yahtzee", 0)
-	board.Set("Chance", 0)
+func GenerateBoard() [13]ScoreItem {
+	board := [13]ScoreItem{}
+
+	board[0] = ScoreItem{"1's", 0}
+	board[1] = ScoreItem{"2's", 0}
+	board[2] = ScoreItem{"3's", 0}
+	board[3] = ScoreItem{"4's", 0}
+	board[4] = ScoreItem{"5's", 0}
+	board[5] = ScoreItem{"6's", 0}
+	board[6] = ScoreItem{"Three-of-a-kind", 0}
+	board[7] = ScoreItem{"Four-of-a-kind", 0}
+	board[8] = ScoreItem{"Full house", 0}
+	board[9] = ScoreItem{"Small straight", 0}
+	board[10] = ScoreItem{"Large straight", 0}
+	board[11] = ScoreItem{"Yahtzee", 0}
+	board[12] = ScoreItem{"Chance", 0}
 
 	return board
 }
 
-func FindPossibleOptions(board *orderedmap.OrderedMap, dice [5]int) {
+// FindPossibleOptions Finds the playable options from the given dice and scoreboard and prints them out.
+// Returns the vector of playable options with "Cross out" in the last position.
+func FindPossibleOptions(board [13]ScoreItem, dice [5]int) []ScoreItem {
 	points := 0
 	for i := 0; i < 5; i++ {
 		points += dice[i]
 	}
 
 	m := DiceMap(dice)
-	possibilities := orderedmap.NewOrderedMap()
+	//possibilities := orderedmap.NewOrderedMap()
+	possibilities := make([]ScoreItem, 0)
 
 	// Calculate upper hand
-	value, _ := board.Get("1's")
-	if m[1] > 0 && value == 0 {
-		possibilities.Set("1's", m[1])
+	// Note: it's m[1] instead of m[0] because 1 references the number, not the position
+	if m[1] > 0 && board[0].points == 0 {
+		possibilities = append(possibilities, ScoreItem{"1's", m[1]})
 	}
-	value, _ = board.Get("2's")
-	if m[2] > 0 && value == 0 {
-		possibilities.Set("2's", m[2]*2)
+
+	if m[2] > 0 && board[1].points == 0 {
+		possibilities = append(possibilities, ScoreItem{"2's", m[2] * 2})
 	}
-	value, _ = board.Get("3's")
-	if m[3] > 0 && value == 0 {
-		possibilities.Set("3's", m[3]*3)
+
+	if m[3] > 0 && board[2].points == 0 {
+		possibilities = append(possibilities, ScoreItem{"3's", m[3] * 3})
 	}
-	value, _ = board.Get("4's")
-	if m[4] > 0 && value == 0 {
-		possibilities.Set("4's", m[4]*4)
+
+	if m[4] > 0 && board[3].points == 0 {
+		possibilities = append(possibilities, ScoreItem{"4's", m[4] * 4})
 	}
-	value, _ = board.Get("5's")
-	if m[5] > 0 && value == 0 {
-		possibilities.Set("5's", m[5]*5)
+
+	if m[5] > 0 && board[4].points == 0 {
+		possibilities = append(possibilities, ScoreItem{"5's", m[5] * 5})
 	}
-	value, _ = board.Get("6's")
-	if m[6] > 0 && value == 0 {
-		possibilities.Set("6's", m[6]*6)
+
+	if m[6] > 0 && board[5].points == 0 {
+		possibilities = append(possibilities, ScoreItem{"6's", m[6] * 6})
 	}
 
 	// Calculate lower hand
-	value, _ = board.Get("Three-of-a-kind")
-	if CalculateThreeKind(m) == 1 && value == 0 {
-		possibilities.Set("Three-of-a-kind", points)
+	if CalculateThreeKind(m) == 1 && board[6].points == 0 {
+		possibilities = append(possibilities, ScoreItem{"Three-of-a-kind", points})
 	}
-	value, _ = board.Get("Four-of-a-kind")
-	if CalculateFourKind(m) == 1 && value == 0 {
-		possibilities.Set("Four-of-a-kind", points)
+
+	if CalculateFourKind(m) == 1 && board[7].points == 0 {
+		possibilities = append(possibilities, ScoreItem{"Four-of-a-kind", points})
 	}
-	value, _ = board.Get("Full house")
-	if CalculateFullHouse(m) == 1 && value == 0 {
-		possibilities.Set("Full house", 25)
+
+	if CalculateFullHouse(m) == 1 && board[8].points == 0 {
+		possibilities = append(possibilities, ScoreItem{"Full house", 25})
 	}
-	value, _ = board.Get("Small straight")
-	if CalculateSmallStraight(dice) == 1 && value == 0 {
-		possibilities.Set("Small straight", 30)
+
+	if CalculateSmallStraight(dice) == 1 && board[9].points == 0 {
+		possibilities = append(possibilities, ScoreItem{"Small straight", 30})
 	}
-	value, _ = board.Get("Large straight")
-	if CalculateLargeStraight(dice) == 1 && value == 0 {
-		possibilities.Set("Large straight", 40)
+
+	if CalculateLargeStraight(dice) == 1 && board[10].points == 0 {
+		possibilities = append(possibilities, ScoreItem{"Large straight", 40})
 	}
 	if CalculateYahtzee(m) == 1 {
 		// A Yahtzee is worth +50 points each time.
 		// The first is worth 50, then 100, then 150, etc.
-		value, _ = board.Get("Yahtzee")
-		yahtzeeValue := value.(int) + 50
-		possibilities.Set("Yahtzee", yahtzeeValue)
+		yahtzeeValue := board[11].points + 50
+		possibilities = append(possibilities, ScoreItem{"Yahtzee", yahtzeeValue})
 	}
 
 	color.Set(color.FgHiYellow)
-	i := 1
-	for _, key := range possibilities.Keys() {
-		val, _ := possibilities.Get(key)
-		fmt.Printf("%d. %s (%d", i, key, val)
-		if val == 1 {
+	var i int
+	for i = 0; i < len(possibilities); i++ {
+		fmt.Printf("%d. %s (%d", i+1, possibilities[i].name, possibilities[i].points)
+		if possibilities[i].points == 1 {
 			fmt.Println(" point)")
 		} else {
 			fmt.Println(" points)")
 		}
-		i++
 	}
 
 	color.Set(color.FgHiRed)
-	fmt.Printf("%d. Cross out\n", i)
+	fmt.Printf("%d. Cross out\n", i+1)
 	color.Set(color.FgHiWhite)
+
+	possibilities = append(possibilities, ScoreItem{"Cross out", 0})
+	return possibilities
 }
