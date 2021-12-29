6ddd40c987c915f5cafb360dd8fd2db98684a496
Camden Mac Leod <camden.macleod@pm.me>
2021-12-28 20:08:26 -0600

    Move to recent changes

--- main.go
+++ main.go
@@ -4,39 +4,16 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
 	fmt.Println("CyanCoding's Yahtzee Hack!")
-	totalSuccesses := 0.0
-	numberOfTrials := 3.0
-	phase1 := Factorial(numberOfTrials) / (Factorial(numberOfTrials-totalSuccesses) * Factorial(totalSuccesses)) * math.Pow(1.0/6.0, totalSuccesses) * math.Pow(5.0/6.0, numberOfTrials-totalSuccesses)
-	totalSuccesses = 1.0
-	phase2 := Factorial(numberOfTrials) / (Factorial(numberOfTrials-totalSuccesses) * Factorial(totalSuccesses)) * math.Pow(1.0/6.0, totalSuccesses) * math.Pow(5.0/6.0, numberOfTrials-totalSuccesses)
-	totalSuccesses = 2.0
-	phase3 := Factorial(numberOfTrials) / (Factorial(numberOfTrials-totalSuccesses) * Factorial(totalSuccesses)) * math.Pow(1.0/6.0, totalSuccesses) * math.Pow(5.0/6.0, numberOfTrials-totalSuccesses)
 
-	value := Factorial(numberOfTrials) / (Factorial(numberOfTrials-totalSuccesses) * Factorial(totalSuccesses)) * math.Pow(1.0/6.0, totalSuccesses) * math.Pow(5.0/6.0, numberOfTrials-totalSuccesses)
-	totalSuccesses = 3.0
-	value += Factorial(numberOfTrials) / (Factorial(numberOfTrials-totalSuccesses) * Factorial(totalSuccesses)) * math.Pow(1.0/6.0, totalSuccesses) * math.Pow(5.0/6.0, numberOfTrials-totalSuccesses)
-
-	totalSuccesses = 1.0
-	numberOfTrials = 2.0
-	value2 := Factorial(numberOfTrials) / (Factorial(numberOfTrials-totalSuccesses) * Factorial(totalSuccesses)) * math.Pow(1.0/6.0, totalSuccesses) * math.Pow(5.0/6.0, numberOfTrials-totalSuccesses)
-	totalSuccesses = 2.0
-	value2 += Factorial(numberOfTrials) / (Factorial(numberOfTrials-totalSuccesses) * Factorial(totalSuccesses)) * math.Pow(1.0/6.0, totalSuccesses) * math.Pow(5.0/6.0, numberOfTrials-totalSuccesses)
-
-	percentage := ((phase1 * value) + (phase2 * value2)) + phase3
-
-	fmt.Println(percentage)
-
+	board := GenerateBoard()
 	score := 0
 
 	// Infinite loop until the game ends
 	for i := 1; i <= 13; i++ {
-		fmt.Println()
-		fmt.Println("------------")
 		fmt.Println("round", i, "- score", score) // round 0 - score 0
 		fmt.Println("Actions:")
 		fmt.Println("1. View current hand")
@@ -50,14 +27,32 @@ func main() {
 		if input == 1 { // View current hand
 
 		} else if input == 2 { // Enter dice
+			var lastRoll, dice [5]int
 			fmt.Print("\033[H\033[2J")
-			for i := 0; i < 3; i++ { // Up to three rolls per turn
-				dice := InputDice()
+			for j := 0; j < 3; j++ { // Up to three rolls per turn
+				dice = InputDice()
 				if dice[0] != 0 {
+					lastRoll = dice
 					CalculateLowerHand(dice)
 				} else {
+					i--
 					break
 				}
+			}
+			fmt.Print("\033[H\033[2J")
+			if lastRoll[0] != 0 {
+				fmt.Printf(
+					"Hand: %d %d %d %d %d\n",
+					lastRoll[0],
+					lastRoll[1],
+					lastRoll[2],
+					lastRoll[3],
+					lastRoll[4],
+				)
+				fmt.Println("Please pick one of the following to fill in.")
+				FindPossibleOptions(board, lastRoll)
+				fmt.Print("Option number > ")
+				_, _ = fmt.Scanln(&input)
 			}
 		} else if input == 3 { // Calculate score
 
